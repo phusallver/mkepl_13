a18f07e4be2e7acd4d94098fb4fd0f9cd77728ef
Muhammad Nagif <103794258+phusallver@users.noreply.github.com>
2024-05-31 19:02:06 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -13,10 +13,6 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
-				
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
 	}
 
 }
